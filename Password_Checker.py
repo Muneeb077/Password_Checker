a3f37982c1a895404a2fcebfcba8a8493462409a
Muneeb Ur Rehman <urehman@mun.ca>
2023-10-31 19:25:09 -0230

    update Password Checker Git repo.

---
+++ Password_Checker.py
@@ -0,0 +1,47 @@
+import requests
+import hashlib
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def password_frontend():
+	return render_template('Password.html')
+
+
+@app.route("/check_password",methods=['POST'])
+def check_password():
+	password = requests.form.get('password')
+	count = pwned_api_check(password)
+	print(count)
+	if count:
+		result = f'{password} was found {count} times... you should change your password'
+	else:
+		result = f'{password} was NOT found.'
+	print(result)
+	return render_template('Password.html', result=result)
+
+def request_api_data(query_char):
+	url = 'https://api.pwnedpasswords.com/range/' + query_char
+	res = requests.get(url)
+	if res.status_code != 200:
+		raise RuntimeError (f'Error fetching: {res.status_code}, check API and try again.')
+	return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+	hashes = (line.split(':') for line in hashes.text.splitlines())
+	for h, count in hashes:
+		if h == hash_to_check:
+			return count
+	return 0
+
+def pwned_api_check(password):
+	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+	first_5letters, tail = sha1password[:5], sha1password[5:]
+	response = request_api_data(first_5letters)
+	# print(first_5letters,tail)
+	return get_password_leaks_count(response,tail)
+
+
+if __name__ == '__main__':
+	app.run(port=5002)
---
+++ static/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordForm = document.getElementById('password-form');
+    const checkButton = document.getElementById('check-button');
+    const resultText = document.getElementById('result');
+
+    passwordForm.addEventListener('submit', async function(e) {
+        e.preventDefault();
+        resultText.innerText = 'Checking...';
+
+        const passwordInput = document.getElementById('password');
+        const password = passwordInput.value;
+
+        const response = await fetch(`/check_password?password=${password}`);
+        const data = await response.json();
+
+        if (data.result) {
+            resultText.innerText = `${password} was found ${data.count} times. You should change your password.`;
+        } else {
+            resultText.innerText = `${password} was NOT found. Carry on!`;
+        }
+    });
+});
